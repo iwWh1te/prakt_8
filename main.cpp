32740c369aaf63869b553f0a7fa395f102bbfcf2
iwWh1te <ylanovru10@gmail.com>
2023-05-27 13:56:35 +0400

    6st_incr

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <QTWidgets>
-
 class MainWindow : public QMainWindow{
 	public:
 		MainWindow();
+		void startButtonClick();
+		void stopButtonClick();
 	private:
 	QLabel *textLabel;
 	QPushButton *startButton;
-	};
-		
+	QPushButton *stopButton;
+	};		
 MainWindow::MainWindow(){
 	resize(900,500);
 	setWindowTitle("MainWin v8.1");
@@ -19,9 +20,17 @@ MainWindow::MainWindow(){
 	startButton = new QPushButton("Start",this);
 	startButton->setGeometry(50,220,70,30);
 	startButton->setFont(buttonFont);
-	
-	}
-
+	stopButton = new QPushButton("Stop",this);
+	stopButton->setGeometry(150,220,70,30);
+	stopButton->setFont(buttonFont);
+	connect (startButton, &QPushButton::clicked,this, &MainWindow::startButtonClick);
+	connect (stopButton, &QPushButton::clicked,this, &MainWindow::stopButtonClick);}	
+void MainWindow::startButtonClick(){
+	textLabel->setText("Start");
+}
+void MainWindow::stopButtonClick(){
+	textLabel->setText("Stop");
+}
 int main(int argc,char *argv[]){
 	QApplication app(argc,argv);
 	MainWindow mainWin;
