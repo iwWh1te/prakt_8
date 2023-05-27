349a76982682125afd103bacd013508a3ee4f04c
iwWh1te <ylanovru10@gmail.com>
2023-05-27 13:43:47 +0400

    4st_incr

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <QTWidgets>
 class MainWindow : public QMainWindow{
 	public:
-		MainWindow();};
-		
+		MainWindow();
+	private:
+	QLabel *textLabel;
+};		
 MainWindow::MainWindow(){
 	resize(900,500);
-setWindowTitle("MainWin v8.1");}
-
+	setWindowTitle("MainWin v8.1");
+	textLabel = new QLabel(this);
+	textLabel->setGeometry(50,100,350,120);
+	textLabel->setText("QLabel test");
+	textLabel->setFont(QFont("Courier",32,QFont::Bold));
+	}
 int main(int argc,char *argv[]){
 	QApplication app(argc,argv);
 	MainWindow mainWin;
