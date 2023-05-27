b7882d456574ee8e823dbb95d3d3d0cdf96dedfe
iwWh1te <ylanovru10@gmail.com>
2023-05-27 13:41:08 +0400

    3st_incr

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <QTWidgets>
+class MainWindow : public QMainWindow{
+	public:
+		MainWindow();};
+		
+MainWindow::MainWindow(){
+	resize(900,500);
+setWindowTitle("MainWin v8.1");}
+
 int main(int argc,char *argv[]){
 	QApplication app(argc,argv);
-	QMainWindow mainWin;
+	MainWindow mainWin;
 	mainWin.show();
 	return app.exec();
 }
